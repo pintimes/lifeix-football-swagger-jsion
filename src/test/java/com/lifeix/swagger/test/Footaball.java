d732d52599fb4e125a2ec7a903a4b92cce9f8ec6
venizeng <venizeng@gmail.com>
2016-09-09 11:50:50 +0800

    修改target的路径

--- src/test/java/com/lifeix/swagger/test/Footaball.java
+++ src/test/java/com/lifeix/swagger/test/Footaball.java
@@ -9,13 +9,18 @@ import com.venizeng.swagger.read.CombineJsons;
 public class Footaball {
 
     public static void main(String[] args) throws Exception {
-        File swaggerFile = new File("src/main/resources/football/app/swagger.json");
-        File definationRoot = new File("src/main/resources/football/definitions");
-        File pathRoot = new File("src/main/resources/football/app/paths");
+    	String root = "src/main/resources/football/";
+        File swaggerFile = new File(root+"app/swagger.json");
+        File pathRoot = new File(root+"app/paths");
+        File definationRoot = new File(root+"definitions");
         String json = new CombineJsons().toSwaggerJson(swaggerFile, definationRoot, pathRoot);
         String path = Thread.currentThread().getContextClassLoader().getResource("").getPath();
         path = path.substring(0, path.indexOf("target"));
-        File file = new File(path + "/football.json");
+        File dir = new File(path + "/swagger");
+        if (!dir.exists()) {
+        	dir.mkdirs();
+		}
+        File file = new File(dir.getAbsolutePath() + "/footballSwagger.json");
         PrintWriter out = new PrintWriter(new FileOutputStream(file));
         out.println(json);
         out.close();
