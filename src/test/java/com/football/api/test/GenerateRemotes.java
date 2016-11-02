b008bb4217b79c44d8172acc6ac5ad6f9410b525
chaochaog <chaochaog@l99.com>
2016-11-02 20:43:36 +0800

    competition项目初始化

--- src/test/java/com/football/api/test/GenerateRemotes.java
+++ src/test/java/com/football/api/test/GenerateRemotes.java
@@ -20,95 +20,97 @@ import jcifs.smb.SmbFileOutputStream;
  *
  */
 public class GenerateRemotes {
-	
-	private static String SMBDIR = "smb://lifeix:lifeix@192.168.50.199/立方网/cfootball/football-api/";
-	public static void main(String[] args) {
-		generateRemote(new File("swagger/comment.json"));
-//		generateAll();
-	}
-	
-	private static void generateAll(){
-		File swaggerDir = new File("swagger/");
-		File[] files = swaggerDir.listFiles();
-		for (File swaggerFile : files) {
-			generateRemote(swaggerFile);
-		}
-	}
-	
-	private static void generateRemote(File swaggerFile){
-		String fileName = FileUtil.getFileName(swaggerFile);
-		/**
-		 * 生成html
-		 */
-		String htmlRoot = "d:/out/" + fileName;
-		SwaggerCodegen.main(new String[] { "generate", "-i", swaggerFile.getAbsolutePath(), "-l", "html", "-o", "d:/out/" + fileName });
-		/**
-		 * 删除remote文件夹
-		 */
-		String remoteDir = SMBDIR + fileName+"/";
-		deleteSMB(remoteDir);
-		/**
-		 * 写入html
-		 */
-		writeSMB(remoteDir, new File(htmlRoot + "/index.html"));
-		/**
-		 * 写入swagger
-		 */
-		writeSMB(remoteDir, swaggerFile);
-	}
 
-	private static void deleteSMB(String dirpath) {
-		try {
-			SmbFile remoteFile = new SmbFile(dirpath);
-			remoteFile.connect(); // 尝试连接
+    private static String SMBDIR = "smb://lifeix:lifeix@192.168.50.199/立方网/cfootball/football-api/";
 
-			SmbFile[] files = remoteFile.listFiles();
-			for (SmbFile smbFile : files) {
-				smbFile.delete();
-			}
-		} catch (Exception e) {
-			String msg = "发生错误：" + e.getLocalizedMessage();
-			System.out.println(msg);
-		}
-	}
+    public static void main(String[] args) {
+        generateRemote(new File("swagger/competition.json"));
+        // generateAll();
+    }
 
-	private static void writeSMB(String dirpath, File file) {
-		InputStream in = null;
-		OutputStream out = null;
-		try {
-			System.out.println(dirpath);
-			SmbFile dir = new SmbFile(dirpath);
-			if (!dir.exists()) {
-				dir.mkdir();
-			}
+    private static void generateAll() {
+        File swaggerDir = new File("swagger/");
+        File[] files = swaggerDir.listFiles();
+        for (File swaggerFile : files) {
+            generateRemote(swaggerFile);
+        }
+    }
 
-			SmbFile remoteFile = new SmbFile(dirpath +file.getName());
-			System.out.println("remoteFilepath ---> " + remoteFile.getPath());
-			remoteFile.connect(); // 尝试连接
+    private static void generateRemote(File swaggerFile) {
+        String fileName = FileUtil.getFileName(swaggerFile);
+        /**
+         * 生成html
+         */
+        String htmlRoot = "d:/out/" + fileName;
+        SwaggerCodegen.main(new String[] { "generate", "-i", swaggerFile.getAbsolutePath(), "-l", "html",
+                "-o", "d:/out/" + fileName });
+        /**
+         * 删除remote文件夹
+         */
+        String remoteDir = SMBDIR + fileName + "/";
+        deleteSMB(remoteDir);
+        /**
+         * 写入html
+         */
+        writeSMB(remoteDir, new File(htmlRoot + "/index.html"));
+        /**
+         * 写入swagger
+         */
+        writeSMB(remoteDir, swaggerFile);
+    }
 
-			in = new BufferedInputStream(new FileInputStream(file));
-			out = new BufferedOutputStream(new SmbFileOutputStream(remoteFile));
+    private static void deleteSMB(String dirpath) {
+        try {
+            SmbFile remoteFile = new SmbFile(dirpath);
+            remoteFile.connect(); // 尝试连接
 
-			byte[] buffer = new byte[4096];
-			int len = 0; // 读取长度
-			while ((len = in.read(buffer, 0, buffer.length)) != -1) {
-				out.write(buffer, 0, len);
-			}
-			out.flush(); // 刷新缓冲的输出流
-		} catch (Exception e) {
-			String msg = "发生错误：" + e.getLocalizedMessage();
-			System.out.println(msg);
-		} finally {
-			try {
-				if (out != null) {
-					out.close();
-				}
-				if (in != null) {
-					in.close();
-				}
-			} catch (Exception e) {
-			}
-		}
-	}
+            SmbFile[] files = remoteFile.listFiles();
+            for (SmbFile smbFile : files) {
+                smbFile.delete();
+            }
+        } catch (Exception e) {
+            String msg = "发生错误：" + e.getLocalizedMessage();
+            System.out.println(msg);
+        }
+    }
+
+    private static void writeSMB(String dirpath, File file) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            System.out.println(dirpath);
+            SmbFile dir = new SmbFile(dirpath);
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+
+            SmbFile remoteFile = new SmbFile(dirpath + file.getName());
+            System.out.println("remoteFilepath ---> " + remoteFile.getPath());
+            remoteFile.connect(); // 尝试连接
+
+            in = new BufferedInputStream(new FileInputStream(file));
+            out = new BufferedOutputStream(new SmbFileOutputStream(remoteFile));
+
+            byte[] buffer = new byte[4096];
+            int len = 0; // 读取长度
+            while ((len = in.read(buffer, 0, buffer.length)) != -1) {
+                out.write(buffer, 0, len);
+            }
+            out.flush(); // 刷新缓冲的输出流
+        } catch (Exception e) {
+            String msg = "发生错误：" + e.getLocalizedMessage();
+            System.out.println(msg);
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e) {
+            }
+        }
+    }
 
 }
